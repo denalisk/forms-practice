b742ae1e38cafa1c65e05c603c4fd7c915fd0a92
Sam Kirsch and Tammy Dang <tammy@>
2017-01-11 09:48:32 -0800

    change the functionality to generate an uppercase response

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var nameInput = $("input#name").val();
-    var streetAddressInput = $("input#streetAddress").val();
-    var cityInput = $("input#city").val();
-    var stateInput = $("input#state").val();
-    var zipcodeInput = $("input#zipcode").val();
+    var contentInput = $("input#content").val();
+    var upperContent = contentInput.toUpperCase();
 
-    $(".name").text(nameInput);
-    $(".streetAddress").text(streetAddressInput);
-    $(".city").text(cityInput);
-    $(".state").text(stateInput);
-    $(".zipcode").text(zipcodeInput);
+    $(".content").text(upperContent);
 
-    $("#letter").show();
+    // $("#reply").show();
 
     event.preventDefault();
 
-  })
-})
+  });
+});
